01ba4e96a8fccf104128d463f99fd22626cf4304
iturres <emanueliturres.co@gmail.com>
2023-10-27 02:06:58 -0300

    Feat: Add project 4 queries to 'queries.sql'
    - Who was the last animal seen by William Tatcher?
    - How many different animals did Stephanie Mendez see?
    - List all vets and their specialties, including vets
      with no specialties.
    - List all animals that visited Stephanie Mendez between
      April 1st and August 30th, 2020.
    - What animal has the most visits to vets?
    - Who was Maisy Smith's first visit?
    - Details for most recent visit: animal information,
      vet information, and date of visit.
    - How many visits were with a vet that did not specialize
      in that animal's species?
    - What specialty should Maisy Smith consider getting?

--- queries.sql
+++ queries.sql
@@ -271,3 +271,132 @@ LIMIT
 
 -- ** Project 3 END.
 -- *
+-- ** Project 4 START.
+-- **** Who was the last animal seen by William Tatcher?
+SELECT
+    animals.name AS last_animal_seen_by_dr_tatcher,
+    visits.date_of_visit AS date_of_visit
+FROM
+    animals
+    JOIN visits ON visits.animal_id = animals.id
+    JOIN vets ON vets.id = visits.vet_id
+WHERE
+    vets.name = 'William Tatcher'
+ORDER BY
+    visits.date_of_visit DESC
+LIMIT
+    1;
+
+-- **** How many different animals did Stephanie Mendez see?
+SELECT DISTINCT
+    COUNT(animals.name) AS diff_animals
+FROM
+    animals
+    JOIN visits ON visits.animal_id = animals.id
+    JOIN vets ON vets.id = visits.vet_id
+WHERE
+    vets.name = 'Stephanie Mendez';
+
+-- **** List all vets and their specializations,
+-- **** including vets with no specialization.
+SELECT
+    vets.name AS vet_name,
+    species.name AS species_name
+FROM
+    vets
+    FULL JOIN specializations ON specializations.vet_id = vets.id
+    FULL JOIN species ON species.id = specializations.species_id;
+
+-- **** List all animals that visited Stephanie Mendez between
+-- **** April 1st and August 30th, 2020.
+SELECT
+    animals.name AS animal_name,
+    visits.date_of_visit AS date_of_visit
+FROM
+    animals
+    JOIN visits ON visits.animal_id = animals.id
+    JOIN vets ON vets.id = visits.vet_id
+WHERE
+    vets.name = 'Stephanie Mendez'
+    AND date_of_visit BETWEEN '2020-04-01' AND '2020-08-30';
+
+-- **** What animal has the most visits to vets?
+SELECT
+    animals.name AS animal_with_most_visits_to_the_vet
+FROM
+    animals
+    JOIN visits ON visits.animal_id = animals.id
+GROUP BY
+    animals.name
+ORDER BY
+    COUNT(visits.animal_id) DESC
+LIMIT
+    1;
+
+-- **** Who was Maisy Smith's first visit?
+SELECT
+    animals.name AS animal_name
+FROM
+    animals
+    JOIN visits ON visits.animal_id = animals.id
+    JOIN vets ON vets.id = visits.vet_id
+WHERE
+    vets.name = 'Maisy Smith'
+ORDER BY
+    visits.date_of_visit
+LIMIT
+    1;
+
+-- **** Details for most recent visit: animal information,
+-- **** vet information, and date of visit.
+SELECT
+    animals.*,
+    vets.*,
+    visits.date_of_visit AS date_of_visit
+FROM
+    animals
+    JOIN visits ON visits.animal_id = animals.id
+    JOIN vets ON vets.id = visits.vet_id
+ORDER BY
+    date_of_visit DESC
+LIMIT
+    1;
+
+-- **** How many visits were with a vet that did not specialize
+-- **** in that animal's species?
+SELECT
+    animals.name AS animal_name,
+    vets.name AS vet_name,
+    COUNT(visits.date_of_visit) AS visit_count
+FROM
+    animals
+    JOIN visits ON visits.animal_id = animals.id
+    JOIN vets ON vets.id = visits.vet_id
+    LEFT JOIN specializations ON spec.vet_id = vets.id
+    AND spec.species_id = animals.species_id
+WHERE
+    spec.vet_id IS NULL
+GROUP BY
+    animal_name,
+    vet_name;
+
+-- **** What specialty should Maisy Smith consider getting?
+-- **** Look for the species she gets the most.
+SELECT
+    species.name AS species_name,
+    COUNT(visits.date_of_visit) AS visit_count
+FROM
+    species
+    JOIN animals ON animals.species_id = species.id
+    JOIN visits ON visits.animal_id = animals.id
+    JOIN vets ON vets.id = visits.vet_id
+WHERE
+    vets.name = 'Maisy Smith'
+GROUP BY
+    species_name
+ORDER BY
+    visit_count DESC
+LIMIT
+    1;
+
+-- ** Project 4 END.
