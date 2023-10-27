be2690b47ad2df2f5d31e04bee8562dd3dbb2ea2
iturres <emanueliturres.co@gmail.com>
2023-10-26 23:29:54 -0300

    Feat: Enhance schema to include new table and junction tables
    - Add 'vets' table.
    - Add 'specializations' junction table for 'species - vets'.
    - Add 'visits' junction table for 'animals - vets'.

--- schema.sql
+++ schema.sql
@@ -35,3 +35,32 @@ ADD CONSTRAINT fk_species FOREIGN KEY (species_id) REFERENCES species (id) ON DE
 ALTER TABLE animals
 ADD COLUMN owner_id SMALLINT,
 ADD CONSTRAINT fk_owner FOREIGN KEY (owner_id) REFERENCES owners (id) ON DELETE CASCADE;
+
+CREATE TABLE
+    vets (
+        id SMALLINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+        name VARCHAR(40) NOT NULL,
+        age SMALLINT NOT NULL,
+        date_of_graduation DATE NOT NULL
+    );
+
+-- * Junction Table for -species- -vets- tables.
+CREATE TABLE
+    specializations (
+        specializations_id SMALLINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+        vet_id SMALLINT,
+        species_id SMALLINT,
+        CONSTRAINT fk_vet FOREIGN KEY (vet_id) REFERENCES vets (id) ON DELETE CASCADE,
+        CONSTRAINT fk_specie FOREIGN KEY (species_id) REFERENCES species (id) ON DELETE CASCADE
+    );
+
+-- * Junction Table for -animals- -vets- tables.
+CREATE TABLE
+    visits (
+        visits_id SMALLINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+        animal_id SMALLINT,
+        vet_id SMALLINT,
+        CONSTRAINT fk_animal FOREIGN KEY (animal_id) REFERENCES animals (id) ON DELETE CASCADE,
+        CONSTRAINT fk_vet FOREIGN KEY (vet_id) REFERENCES vets (id) ON DELETE CASCADE,
+        date_of_visit DATE NOT NULL
+    );
